d21d58798658bc676fba16f23bfad7678f18af1b
David Requeno <davidrequeno52@gmail.com>
2025-05-23 22:46:35 -0600

    Agregar comentario descriptivo en CreateProductRequestDto para clarificar el propósito del DTO en la creación de productos.

--- internal/services/catalogwriteservice/internal/products/features/creatingproduct/dtos/create_product_request_dto.go
+++ internal/services/catalogwriteservice/internal/products/features/creatingproduct/dtos/create_product_request_dto.go
@@ -1,5 +1,6 @@
 package dtos
 
+// Request específico - solo lo que el cliente envía
 type CreateProductRequestDto struct {
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
