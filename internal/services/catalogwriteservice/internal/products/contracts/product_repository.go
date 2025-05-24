5f9caf730f2e8a8c861e3e73a1870b3c0f7d4d54
David Requeno <davidrequeno52@gmail.com>
2025-05-23 22:24:02 -0600

    Agregar métodos GetProductById, UpdateProduct y DeleteProductByID a la interfaz ProductRepository en el servicio de catálogo.

--- internal/services/catalogwriteservice/internal/products/contracts/product_repository.go
+++ internal/services/catalogwriteservice/internal/products/contracts/product_repository.go
@@ -4,8 +4,12 @@ import (
 	"context"
 
 	"github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/models"
+	uuid "github.com/satori/go.uuid"
 )
 
 type ProductRepository interface {
 	CreateProduct(ctx context.Context, product *models.Product) (*models.Product, error)
+	GetProductById(ctx context.Context, uuid uuid.UUID) (*models.Product, error)
+	UpdateProduct(ctx context.Context, product *models.Product) (*models.Product, error)
+	DeleteProductByID(ctx context.Context, uuid uuid.UUID) error
 }
