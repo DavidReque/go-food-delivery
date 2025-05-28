97934be41b488a895d0db75ed8c7bd6c7d5e2756
David Requeno <davidrequeno52@gmail.com>
2025-05-28 00:04:52 -0600

    Actualizar dependencias en go.mod y go.sum, incluyendo nuevas bibliotecas para la validación y manejo de productos, así como mejoras en la gestión de métricas y errores. Agregar la estructura CreateProductEndpoint para integrar parámetros de ruta en la creación de productos.

--- internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1/create_product_endpoint.go
+++ internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1/create_product_endpoint.go
@@ -1,1 +1,7 @@
 package v1
+
+import "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/dtos/v1/fxparams"
+
+type CreateProductEndpoint struct {
+	fxparams.ProductRouteParams
+}
