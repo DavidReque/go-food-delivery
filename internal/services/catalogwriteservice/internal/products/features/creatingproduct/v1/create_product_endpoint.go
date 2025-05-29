25dbe240bcdd71ec7f51ae64806f4ad04dd118ca
David Requeno <davidrequeno52@gmail.com>
2025-05-29 00:01:30 -0600

    Implementar el endpoint para la creación de productos, incluyendo la lógica de manejo de solicitudes y respuestas, así como la validación de datos. Se agrega la estructura createProductEndpoint y se define el método handler para procesar las peticiones.

--- internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1/create_product_endpoint.go
+++ internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1/create_product_endpoint.go
@@ -1,7 +1,66 @@
 package v1
 
-import "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/dtos/v1/fxparams"
+import (
+	"net/http"
 
-type CreateProductEndpoint struct {
+	"github.com/DavidReque/go-food-delivery/internal/pkg/core/web/route"
+	customErrors "github.com/DavidReque/go-food-delivery/internal/pkg/http/httperrors/customerrors"
+	"github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/dtos/v1/fxparams"
+	"github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1/dtos"
+
+	"emperror.dev/errors"
+	"github.com/labstack/echo/v4"
+	"github.com/mehdihadeli/go-mediatr"
+)
+
+type createProductEndpoint struct {
 	fxparams.ProductRouteParams
+}
+
+func NewCreteProductEndpoint(
+	params fxparams.ProductRouteParams,
+) route.Endpoint {
+	return &createProductEndpoint{ProductRouteParams: params}
+}
+
+func (ep *createProductEndpoint) MapEndpoint() {
+	ep.ProductsGroup.POST("", ep.handler())
+}
+
+func (ep *createProductEndpoint) handler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := c.Request().Context()
+
+		request := dtos.CreateProductRequestDto{}
+		if err := c.Bind(request); err != nil {
+			badRequestErr := customErrors.NewBadRequestError(
+				err,
+				"error in the binding request",
+			)
+
+			return badRequestErr
+		}
+
+		command, err := NewCreateProductWithValidation(
+			request.Name,
+			request.Description,
+			request.Price,
+		)
+		if err != nil {
+			return err
+		}
+
+		result, err := mediatr.Send[*CreateProduct, *dtos.CreateProductResponseDto](
+			ctx,
+			command,
+		)
+		if err != nil {
+			return errors.WithMessage(
+				err,
+				"error in sending CreateProduct",
+			)
+		}
+
+		return c.JSON(http.StatusCreated, result)
+	}
 }
