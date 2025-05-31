8da1117a8ea7d8d9bf78627bbcff8c61b0a537a6
David Requeno <davidrequeno52@gmail.com>
2025-05-31 00:21:49 -0600

    Actualizar comentarios en el archivo log_type.go para proporcionar descripciones más claras sobre los tipos de logger (Zap y Logrus), mejorando la documentación del código.

--- internal/pkg/logger/models/log_type.go
+++ internal/pkg/logger/models/log_type.go
@@ -4,6 +4,6 @@ type LogType int32
 
 // LogType representa el tipo de logger que se utilizará
 const (
-	Zap    LogType = 0
-	Logrus LogType = 1
+	Zap    LogType = 0 // Zap es el tipo de logger que se utilizará
+	Logrus LogType = 1 // Logrus es el tipo de logger que se utilizará
 )
