68b893cf6e2b81e258363c61819486e29b93fb81
David Requeno <davidrequeno52@gmail.com>
2025-06-02 23:14:45 -0600

    Agregar importación de fxparams en create_product_handler.go, integrando ProductHandlerParams en la estructura del manejador de creación de productos.

--- internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1/create_product_handler.go
+++ internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1/create_product_handler.go
@@ -1,6 +1,7 @@
 package v1
 
+import "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/dtos/v1/fxparams"
 
 type createProductHandler struct {
-	
+	fxparams.ProductHandlerParams
 }
