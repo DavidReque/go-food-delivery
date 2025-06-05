872faa7b9b7a22cfa3ad670e41f80087c99030f2
David Requeno <davidrequeno52@gmail.com>
2025-06-04 22:57:41 -0600

    Actualizar la estructura del evento ProductCreatedV1 para incluir un puntero a types.Message, mejorando la gestión de mensajes al crear un nuevo producto. Se añade la generación de un UUID para cada mensaje creado.

--- internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1/events/integrationevents/product_created.go
+++ internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1/events/integrationevents/product_created.go
@@ -1,15 +1,20 @@
 package integrationevents
 
-import dtoV1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/dtos/v1"
+import (
+	"github.com/DavidReque/go-food-delivery/internal/pkg/core/messaging/types"
+	dtoV1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/dtos/v1"
+
+	uuid "github.com/satori/go.uuid"
+)
 
 type ProductCreatedV1 struct {
-	//*types.Message
+	*types.Message
 	*dtoV1.ProductDto
 }
 
 func NewProductCreatedV1(productDto *dtoV1.ProductDto) *ProductCreatedV1 {
 	return &ProductCreatedV1{
 		ProductDto: productDto,
-		//Message:    types.NewMessage(uuid.NewV4().String()),
+		Message:    types.NewMessage(uuid.NewV4().String()),
 	}
 }
