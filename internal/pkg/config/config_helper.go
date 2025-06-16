4d8c67a031b1a9266ab6871a36f3574339ae3184
David Requeno <davidrequeno52@gmail.com>
2025-06-15 21:45:05 -0600

    Agregar la función BindConfigKey en config_helper.go para vincular claves de configuración a estructuras utilizando viper. Esto mejora la gestión de la configuración en el paquete config.

--- internal/pkg/config/config_helper.go
+++ internal/pkg/config/config_helper.go
@@ -1,1 +1,10 @@
 package config
+
+import "github.com/spf13/viper"
+
+// BindConfigKey vincula una clave de configuración a una estructura
+// key: nombre de la clave en la configuración
+// cfg: puntero a la estructura donde se almacenará la configuración
+func BindConfigKey(key string, cfg interface{}) error {
+	return viper.UnmarshalKey(key, cfg)
+}
