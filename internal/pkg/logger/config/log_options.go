3ee371a1b7246d703b9cca21bcedfd2068343b00
David Requeno <davidrequeno52@gmail.com>
2025-06-15 21:44:35 -0600

    Agregar opciones de configuración para el logger en log_options.go. Se incluye el nivel de log, tipo de logger, habilitación de caller y tracing, y se implementa la función ProvideLogConfig para vincular la configuración desde el entorno.

--- internal/pkg/logger/config/log_options.go
+++ internal/pkg/logger/config/log_options.go
@@ -1,9 +1,29 @@
 package config
 
-import "github.com/DavidReque/go-food-delivery/internal/pkg/logger/models"
+import (
+	"github.com/DavidReque/go-food-delivery/internal/pkg/config"
+	"github.com/DavidReque/go-food-delivery/internal/pkg/config/environment"
+	"github.com/DavidReque/go-food-delivery/internal/pkg/logger/models"
+)
+
+// Nombre de la opción de configuración para el logger
+var optionName = "logOptions"
 
 // LogOptions representa las opciones de configuración para el logger
 type LogOptions struct {
-	LogType       models.LogType // Tipo de logger a utilizar
-	CallerEnabled bool            // Indica si se debe incluir el nombre del archivo y línea en los logs
+	LogLevel       string         `mapstructure:"level"`
+	LogType        models.LogType `mapstructure:"logType"`                      // Tipo de logger a utilizar
+	CallerEnabled  bool           `mapstructure:"callerEnabled"`                // Indica si se debe incluir el nombre del archivo y línea en los logs
+	EnabledTracing bool           `mapstructure:"enableTracing" default:"true"` // Indica si se debe incluir el tracing en los logs
+}
+
+// ProvideLogConfig proporciona las opciones de configuración para el logger
+// env: entorno de la aplicación
+// devuelve las opciones de configuración para el logger
+func ProvideLogConfig(env environment.Environment) (*LogOptions, error) {
+	options := &LogOptions{}
+	if err := config.BindConfigKey(optionName, options); err != nil {
+		return nil, err
+	}
+	return options, nil
 }
