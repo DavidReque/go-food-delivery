609f3f6aa2faeed3c2e9408a382b50fe67ac3f96
David Requeno <davidrequeno52@gmail.com>
2025-06-18 23:41:45 -0600

    Agregar el paquete de validación con la interfaz Validator y el pipeline de validación que maneja la validación de solicitudes en el contexto de mediatr.

---
+++ internal/pkg/validation/pipeline/validation_pipeline.go
@@ -0,0 +1,33 @@
+package pipeline
+
+import (
+	"context"
+
+	"github.com/DavidReque/go-food-delivery/internal/pkg/logger"
+	"github.com/DavidReque/go-food-delivery/internal/pkg/validation"
+	"github.com/mehdihadeli/go-mediatr"
+)
+
+type mediatorValidationPipeline struct {
+	logger logger.Logger
+}
+
+func NewMediatorValidationPipeline(l logger.Logger) mediatr.PipelineBehavior {
+	return &mediatorValidationPipeline{logger: l}
+}
+
+func (m mediatorValidationPipeline) Handle(
+	ctx context.Context,
+	request interface{},
+	next mediatr.RequestHandlerFunc,
+) (interface{}, error) {
+	v, ok := request.(validation.Validator)
+	if ok {
+		err := v.Validate()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return next(ctx)
+}
