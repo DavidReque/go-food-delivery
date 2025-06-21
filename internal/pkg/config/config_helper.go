446cc95ce26e1323507f5e481add2c10f4ebda25
David Requeno <davidrequeno52@gmail.com>
2025-06-20 23:00:49 -0600

    Modificar la función BindConfigKey para que acepte un tipo genérico T y retorne un puntero a una nueva instancia de T con los valores de configuración, mejorando la flexibilidad en la vinculación de claves de configuración.

--- internal/pkg/config/config_helper.go
+++ internal/pkg/config/config_helper.go
@@ -2,9 +2,13 @@ package config
 
 import "github.com/spf13/viper"
 
-// BindConfigKey vincula una clave de configuración a una estructura
-// key: nombre de la clave en la configuración
-// cfg: puntero a la estructura donde se almacenará la configuración
-func BindConfigKey(key string, cfg interface{}) error {
-	return viper.UnmarshalKey(key, cfg)
+// BindConfigKey vincula una clave de configuración a una estructura de tipo genérico T.
+// Retorna un puntero a una nueva instancia de T con los valores de configuración.
+// key: nombre de la clave en la configuración.
+func BindConfigKey[T any](key string) (*T, error) {
+	var cfg T
+	if err := viper.UnmarshalKey(key, &cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
 }
