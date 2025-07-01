fa59f6a847afa7b1c0fd9b167da7632b488123c2
David Requeno <davidrequeno52@gmail.com>
2025-06-30 22:56:55 -0600

    Refactorizar el paquete CQRS eliminando implementaciones de TypeInfo y Request, y creando nuevas estructuras para Query y QueryHandler, mejorando la gestión de consultas en el sistema.

--- internal/pkg/core/cqrs/query.go
+++ internal/pkg/core/cqrs/query.go
@@ -1,32 +1,30 @@
 package cqrs
 
-type command struct {
+type query struct {
 	TypeInfo
 	Request
 }
 
-type Command interface {
-	isCommand()
+type Query interface {
+	isQuery()
 
 	Request
 	TypeInfo
 }
 
-func NewCommandByT[T any]() Command {
-	c := &command{
+func NewQueryByT[T any]() Query {
+	return &query{
 		TypeInfo: NewTypeInfoT[T](),
 		Request:  NewRequest(),
 	}
-
-	return c
 }
 
 // https://github.com/EventStore/EventStore-Client-Go/blob/master/esdb/position.go#L29
-func (c *command) isCommand() {
+func (q *query) isQuery() {
 }
 
-func IsCommand(obj interface{}) bool {
-	if _, ok := obj.(Command); ok {
+func IsQuery(obj interface{}) bool {
+	if _, ok := obj.(Query); ok {
 		return true
 	}
 
