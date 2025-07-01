312b5768b887f39970689ed0c61147504a17b4b1
David Requeno <davidrequeno52@gmail.com>
2025-06-30 22:46:13 -0600

    Agregar el archivo type_info.go que define la interfaz TypeInfo y su implementación, proporcionando funciones para obtener el nombre corto, el nombre completo y el tipo reflejado, facilitando la gestión de tipos genéricos en el sistema.

---
+++ internal/pkg/core/cqrs/type_info.go
@@ -0,0 +1,43 @@
+package cqrs
+
+import (
+	"reflect"
+
+	"github.com/DavidReque/go-food-delivery/internal/pkg/reflection/typemapper"
+)
+
+type TypeInfo interface {
+	ShortTypeName() string
+	FullTypeName() string
+	Type() reflect.Type
+}
+
+type typeInfo struct {
+	shortTypeName string
+	fullTypeName  string
+	typ           reflect.Type
+}
+
+func NewTypeInfoT[T any]() TypeInfo {
+	name := typemapper.GetGenericTypeNameByT[T]()
+	fullName := typemapper.GetGenericFullTypeNameByT[T]()
+	typ := typemapper.GetGenericTypeByT[T]()
+
+	return &typeInfo{
+		fullTypeName:  fullName,
+		typ:           typ,
+		shortTypeName: name,
+	}
+}
+
+func (t *typeInfo) ShortTypeName() string {
+	return t.shortTypeName
+}
+
+func (t *typeInfo) FullTypeName() string {
+	return t.fullTypeName
+}
+
+func (t *typeInfo) Type() reflect.Type {
+	return t.typ
+}
