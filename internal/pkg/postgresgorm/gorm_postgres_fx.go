9170d72f445616f5e552ac6a5d9ba3cbd206bcb8
David Requeno <davidrequeno52@gmail.com>
2025-07-14 22:50:55 -0600

    Agregar archivos para la gestión de opciones de GORM y verificación de salud en PostgreSQL, incluyendo pruebas unitarias y la implementación de un verificador de salud que utiliza la conexión a la base de datos, mejorando la estructura y funcionalidad del módulo postgresgorm.

---
+++ internal/pkg/postgresgorm/gorm_postgres_fx.go
@@ -0,0 +1,23 @@
+package postgresgorm
+
+import (
+	"fmt"
+
+	"github.com/DavidReque/go-food-delivery/internal/pkg/health/contracts"
+	"go.uber.org/fx"
+)
+
+var Module = fx.Module(
+	"gormpostgresfx",
+	fx.Provide(
+		provideConfig,
+		NewGorm,
+		NewSQLDB,
+
+		fx.Annotate(
+			NewGormHealthChecker,
+			fx.As(new(contracts.Health)),
+			fx.ResultTags(fmt.Sprintf(`group: %s`, "healths")),
+		),
+	),
+)
