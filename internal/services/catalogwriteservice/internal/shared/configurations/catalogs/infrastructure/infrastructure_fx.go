8b0328435c8701e7a107c7ec932878900aa27782
David Requeno <davidrequeno52@gmail.com>
2025-08-05 22:05:38 -0600

    Agregar la configuración de RabbitMQ en el módulo de infraestructura, incluyendo la integración de la función para configurar productos en el sistema de mensajería.

--- internal/services/catalogwriteservice/internal/shared/configurations/catalogs/infrastructure/infrastructure_fx.go
+++ internal/services/catalogwriteservice/internal/shared/configurations/catalogs/infrastructure/infrastructure_fx.go
@@ -5,6 +5,9 @@ import (
 	"github.com/DavidReque/go-food-delivery/internal/pkg/grpc"
 	"github.com/DavidReque/go-food-delivery/internal/pkg/migration/goose"
 	"github.com/DavidReque/go-food-delivery/internal/pkg/postgresmessaging"
+	"github.com/DavidReque/go-food-delivery/internal/pkg/rabbitmq"
+	"github.com/DavidReque/go-food-delivery/internal/pkg/rabbitmq/configurations"
+	rabbitmq2 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/configurations/rabbitmq"
 
 	"github.com/DavidReque/go-food-delivery/internal/pkg/http/customecho"
 	"github.com/DavidReque/go-food-delivery/internal/pkg/postgresgorm"
@@ -24,4 +27,11 @@ var Module = fx.Module(
 	postgresgorm.Module,
 	postgresmessaging.Module,
 	goose.Module,
+	rabbitmq.ModuleFunc(
+		func() configurations.RabbitMQConfigurationBuilderFuc {
+			return func(builder configurations.RabbitMQConfigurationBuilder) {
+				rabbitmq2.ConfigProductsRabbitMQ(builder)
+			}
+		},
+	),
 )
