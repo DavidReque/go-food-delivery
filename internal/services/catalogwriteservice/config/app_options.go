a4430a37aec14d3277fa4b4fabf382d2516d51ba
David Requeno <davidrequeno52@gmail.com>
2025-08-14 22:28:21 -0600

    Agregar la implementación de opciones de configuración para el servicio de catálogo, incluyendo la definición de la estructura AppOptions y su integración en el módulo de configuración. También se actualiza el archivo de configuración de desarrollo con nuevas opciones para el servicio y trazabilidad.

---
+++ internal/services/catalogwriteservice/config/app_options.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"strings"
+
+	"github.com/DavidReque/go-food-delivery/internal/pkg/config"
+	"github.com/DavidReque/go-food-delivery/internal/pkg/config/environment"
+	typeMapper "github.com/DavidReque/go-food-delivery/internal/pkg/reflection/typemapper"
+	"github.com/iancoleman/strcase"
+)
+
+type AppOptions struct {
+	DeliveryType string `mapstructure:"deliveryType" env:"DeliveryType"`
+	ServiceName  string `mapstructure:"serviceName"  env:"serviceName"`
+}
+
+func NewAppOptions(environment environment.Environment) (*AppOptions, error) {
+	optionName := strcase.ToLowerCamel(typeMapper.GetGenericTypeNameByT[AppOptions]())
+	cfg, err := config.BindConfigKey[AppOptions](optionName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
+func (cfg *AppOptions) GetMicroserviceNameUpper() string {
+	return strings.ToUpper(cfg.ServiceName)
+}
+
+func (cfg *AppOptions) GetMicroserviceName() string {
+	return cfg.ServiceName
+}
