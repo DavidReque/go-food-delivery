3fbb2b7c58ce066f676c8d346b9d3fed13091238
David Requeno <davidrequeno52@gmail.com>
2025-08-18 22:26:12 -0600

    Agregar archivo de configuración .air.toml y nuevos manejadores y endpoints para la gestión de productos en el servicio de catálogo. Se implementan funcionalidades para obtener, actualizar, y eliminar productos, junto con la validación de entradas y la lógica para manejar errores. También se corrigen importaciones y se mejora la estructura del código.

--- internal/services/catalogwriteservice/internal/products/products_fx.go
+++ internal/services/catalogwriteservice/internal/products/products_fx.go
@@ -6,6 +6,11 @@ import (
 	"github.com/DavidReque/go-food-delivery/internal/pkg/http/customecho/contracts"
 	"github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/data/repositories"
 	creatingproductv1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1"
+	deletingproductv1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/deletingproduct/v1"
+	gettingproductbyidv1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/gettingproductbyid/v1"
+	gettingproductsv1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/gettingproducts/v1"
+	searchingproductsv1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/searchingproduct/v1"
+	updatingoroductsv1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/updatingproduct/v1"
 	"github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/shared/grpc"
 	"github.com/labstack/echo/v4"
 	"go.uber.org/fx"
@@ -52,12 +57,52 @@ var Module = fx.Module(
 			creatingproductv1.NewCreateProductHandler,
 			"product-handlers",
 		),
+		cqrs.AsHandler(
+			gettingproductsv1.NewGetProductsHandler,
+			"product-handlers",
+		),
+		cqrs.AsHandler(
+			deletingproductv1.NewDeleteProductHandler,
+			"product-handlers",
+		),
+		cqrs.AsHandler(
+			gettingproductbyidv1.NewGetProductByIDHandler,
+			"product-handlers",
+		),
+		cqrs.AsHandler(
+			searchingproductsv1.NewSearchProductsHandler,
+			"product-handlers",
+		),
+		cqrs.AsHandler(
+			updatingoroductsv1.NewUpdateProductHandler,
+			"product-handlers",
+		),
 	),
 
 	// add endpoints to DI
 	fx.Provide(
 		route.AsRoute(
 			creatingproductv1.NewCreteProductEndpoint,
+			"product-routes",
+		),
+		route.AsRoute(
+			updatingoroductsv1.NewUpdateProductEndpoint,
+			"product-routes",
+		),
+		route.AsRoute(
+			gettingproductsv1.NewGetProductsEndpoint,
+			"product-routes",
+		),
+		route.AsRoute(
+			searchingproductsv1.NewSearchProductsEndpoint,
+			"product-routes",
+		),
+		route.AsRoute(
+			gettingproductbyidv1.NewGetProductByIdEndpoint,
+			"product-routes",
+		),
+		route.AsRoute(
+			deletingproductv1.NewDeleteProductEndpoint,
 			"product-routes",
 		),
 	),
--- internal/services/catalogwriteservice/internal/shared/grpc/product_grpc_service_server.go
+++ internal/services/catalogwriteservice/internal/shared/grpc/product_grpc_service_server.go
@@ -6,15 +6,20 @@ import (
 
 	customErrors "github.com/DavidReque/go-food-delivery/internal/pkg/http/httperrors/customerrors"
 	"github.com/DavidReque/go-food-delivery/internal/pkg/logger"
+	"github.com/DavidReque/go-food-delivery/internal/pkg/mapper"
 	"github.com/DavidReque/go-food-delivery/internal/pkg/otel/tracing/attribute"
 	createProductCommandV1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1"
 	createProductDtosV1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/creatingproduct/v1/dtos"
+	getProductByIdQueryV1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/gettingproductbyid/v1"
+	getProductByIdDtosV1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/gettingproductbyid/v1/dtos"
+	updateProductCommandV1 "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/products/features/updatingproduct/v1"
 	"github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/shared/contracts"
 	productsService "github.com/DavidReque/go-food-delivery/internal/services/catalogwriteservice/internal/shared/grpc/genproto"
-	"github.com/mehdihadeli/go-mediatr"
-	"github.com/pkg/errors"
 
-	//uuid "github.com/satori/go.uuid"
+	"emperror.dev/errors"
+	"github.com/google/uuid"
+	"github.com/mehdihadeli/go-mediatr"
+	satori_uuid "github.com/satori/go.uuid"
 	attribute2 "go.opentelemetry.io/otel/attribute"
 	api "go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/trace"
@@ -27,9 +32,11 @@ var grpcMetricsAttr = api.WithAttributes(
 type ProductGrpcServiceServer struct {
 	catalogsMetrics *contracts.CatalogsMetrics
 	logger          logger.Logger
+	// Ref:https://github.com/grpc/grpc-go/issues/3794#issuecomment-720599532
+	// product_service_client.UnimplementedProductsServiceServer
 }
 
-// NewProductGrpcService crea una nueva instancia del servidor gRPC para la gestión de productos
+// NewProductGrpcService crea un nuevo servidor de gRPC para el servicio de productos
 func NewProductGrpcService(
 	catalogsMetrics *contracts.CatalogsMetrics,
 	logger logger.Logger,
@@ -40,32 +47,30 @@ func NewProductGrpcService(
 	}
 }
 
-// CreateProduct es un método del servidor gRPC que maneja la creación de nuevos productos.
-// Recibe un contexto y una solicitud de creación de producto, y devuelve una respuesta con el ID del producto creado.
+// CreateProduct crea un nuevo producto
 func (s *ProductGrpcServiceServer) CreateProduct(
 	ctx context.Context,
 	req *productsService.CreateProductReq,
 ) (*productsService.CreateProductRes, error) {
-	// Obtiene el span de tracing del contexto para monitoreo y observabilidad
+	// Obtener el span de la solicitud
 	span := trace.SpanFromContext(ctx)
-	// Agrega los datos de la solicitud como atributos del span
+	// Establecer los atributos del span
 	span.SetAttributes(attribute.Object("Request", req))
-	// Incrementa el contador de métricas para las solicitudes gRPC de creación de productos
+	// Incrementar el contador de solicitudes de gRPC
 	s.catalogsMetrics.CreateProductGrpcRequests.Add(ctx, 1, grpcMetricsAttr)
 
-	// Crea y valida un nuevo comando de creación de producto con los datos de la solicitud
+	// Crear el comando para crear un producto
 	command, err := createProductCommandV1.NewCreateProductWithValidation(
 		req.GetName(),
 		req.GetDescription(),
 		req.GetPrice(),
 	)
 	if err != nil {
-		// Si la validación falla, crea un error personalizado con contexto adicional
+		// Crear un error de validación
 		validationErr := customErrors.NewValidationErrorWrap(
 			err,
 			"[ProductGrpcServiceServer_CreateProduct.StructCtx] command validation failed",
 		)
-		// Registra el error de validación en los logs
 		s.logger.Errorf(
 			fmt.Sprintf(
 				"[ProductGrpcServiceServer_CreateProduct.StructCtx] err: %v",
@@ -75,19 +80,16 @@ func (s *ProductGrpcServiceServer) CreateProduct(
 		return nil, validationErr
 	}
 
-	// Envía el comando validado al mediador para su procesamiento
-	// mediatr.Send es un patrón mediador que maneja la lógica de negocio
+	// Enviar el comando para crear un producto
 	result, err := mediatr.Send[*createProductCommandV1.CreateProduct, *createProductDtosV1.CreateProductResponseDto](
 		ctx,
 		command,
 	)
 	if err != nil {
-		// Si ocurre un error durante el procesamiento, agrega contexto adicional
 		err = errors.WithMessage(
 			err,
 			"[ProductGrpcServiceServer_CreateProduct.Send] error in sending CreateProduct",
 		)
-		// Registra el error con información adicional incluyendo el ID del producto
 		s.logger.Errorw(
 			fmt.Sprintf(
 				"[ProductGrpcServiceServer_CreateProduct.Send] id: {%s}, err: %v",
@@ -99,42 +101,31 @@ func (s *ProductGrpcServiceServer) CreateProduct(
 		return nil, err
 	}
 
-	// Si todo es exitoso, devuelve una respuesta con el ID del producto creado
 	return &productsService.CreateProductRes{
 		ProductId: result.ProductID.String(),
 	}, nil
 }
 
-func (s *ProductGrpcServiceServer) GetProductById(
-	ctx context.Context,
-	req *productsService.GetProductByIdReq,
-) (*productsService.GetProductByIdRes, error) {
-	// TODO: Implement GetProductById
-	return nil, errors.New("GetProductById not implemented")
-}
-
+// UpdateProduct actualiza un producto
 func (s *ProductGrpcServiceServer) UpdateProduct(
 	ctx context.Context,
 	req *productsService.UpdateProductReq,
 ) (*productsService.UpdateProductRes, error) {
-	/*// Obtiene el span de tracing del contexto para monitoreo y observabilidad
+	// Incrementar el contador de solicitudes de gRPC
 	s.catalogsMetrics.UpdateProductGrpcRequests.Add(ctx, 1, grpcMetricsAttr)
-
-	// Obtiene el span de tracing del contexto para monitoreo y observabilidad
+	// Obtener el span de la solicitud
 	span := trace.SpanFromContext(ctx)
-	// Agrega los datos de la solicitud como atributos del span
+	// Establecer los atributos del span
 	span.SetAttributes(attribute.Object("Request", req))
 
-	// Convierte el ID del producto a un UUID
-	productUUID, err := uuid.FromString(req.GetProductId())
-	// Si ocurre un error al convertir el ID del producto a un UUID, se retorna un error de bad request
+	// Convertir el UUID de la solicitud a UUID
+	productUUID, err := satori_uuid.FromString(req.GetProductId())
 	if err != nil {
-		// Crea un error de bad request con stack trace
+		// Crear un error de validación
 		badRequestErr := customErrors.NewBadRequestErrorWrap(
 			err,
 			"[ProductGrpcServiceServer_UpdateProduct.uuid.FromString] error in converting uuid",
 		)
-		// Registra el error de bad request en los logs
 		s.logger.Errorf(
 			fmt.Sprintf(
 				"[ProductGrpcServiceServer_UpdateProduct.uuid.FromString] err: %v",
@@ -143,12 +134,133 @@ func (s *ProductGrpcServiceServer) UpdateProduct(
 		)
 		return nil, badRequestErr
 	}
+
+	// Convertir satori UUID a google UUID
+	googleUUID, err := uuid.Parse(productUUID.String())
+	if err != nil {
+		return nil, customErrors.NewBadRequestErrorWrap(err, "invalid product ID format")
+	}
+
+	// Crear el comando para actualizar un producto
 	command, err := updateProductCommandV1.NewUpdateProductWithValidation(
-		productUUID,
+		googleUUID,
 		req.GetName(),
 		req.GetDescription(),
 		req.GetPrice(),
-	)*/
-	return nil, errors.New("UpdateProduct not implemented")
+	)
+	if err != nil {
+		// Crear un error de validación
+		validationErr := customErrors.NewValidationErrorWrap(
+			err,
+			"[ProductGrpcServiceServer_UpdateProduct.StructCtx] command validation failed",
+		)
+		s.logger.Errorf(
+			fmt.Sprintf(
+				"[ProductGrpcServiceServer_UpdateProduct.StructCtx] err: %v",
+				validationErr,
+			),
+		)
+		return nil, validationErr
+	}
+
+	// Enviar el comando para actualizar un producto
+	if _, err = mediatr.Send[*updateProductCommandV1.UpdateProduct, *mediatr.Unit](ctx, command); err != nil {
+		err = errors.WithMessage(
+			err,
+			"[ProductGrpcServiceServer_UpdateProduct.Send] error in sending CreateProduct",
+		)
+		s.logger.Errorw(
+			fmt.Sprintf(
+				"[ProductGrpcServiceServer_UpdateProduct.Send] id: {%s}, err: %v",
+				command.ProductID,
+				err,
+			),
+			logger.Fields{"Id": command.ProductID},
+		)
+		return nil, err
+	}
+
+	return &productsService.UpdateProductRes{}, nil
 }
 
+// GetProductById obtiene un producto por su ID
+func (s *ProductGrpcServiceServer) GetProductById(
+	ctx context.Context,
+	req *productsService.GetProductByIdReq,
+) (*productsService.GetProductByIdRes, error) {
+	//// we could use trace manually, but I used grpc middleware for doing this
+	//ctx, span, clean := grpcTracing.StartGrpcServerTracerSpan(ctx, "ProductGrpcServiceServer.GetProductById")
+	//defer clean()
+
+	// Incrementar el contador de solicitudes de gRPC
+	s.catalogsMetrics.GetProductByIdGrpcRequests.Add(ctx, 1, grpcMetricsAttr)
+	// Obtener el span de la solicitud
+	span := trace.SpanFromContext(ctx)
+	// Establecer los atributos del span
+	span.SetAttributes(attribute.Object("Request", req))
+
+	// Convertir el UUID de la solicitud a UUID
+	productUUID, err := satori_uuid.FromString(req.GetProductId())
+	if err != nil {
+		// Crear un error de validación
+		badRequestErr := customErrors.NewBadRequestErrorWrap(
+			err,
+			"[ProductGrpcServiceServer_GetProductById.uuid.FromString] error in converting uuid",
+		)
+		s.logger.Errorf(
+			fmt.Sprintf(
+				"[ProductGrpcServiceServer_GetProductById.uuid.FromString] err: %v",
+				badRequestErr,
+			),
+		)
+		return nil, badRequestErr
+	}
+
+	query, err := getProductByIdQueryV1.NewGetProductByIdWithValidation(productUUID)
+	if err != nil {
+		validationErr := customErrors.NewValidationErrorWrap(
+			err,
+			"[ProductGrpcServiceServer_GetProductById.StructCtx] query validation failed",
+		)
+		s.logger.Errorf(
+			fmt.Sprintf(
+				"[ProductGrpcServiceServer_GetProductById.StructCtx] err: %v",
+				validationErr,
+			),
+		)
+		return nil, validationErr
+	}
+
+	// Enviar el comando para obtener un producto por su ID
+	queryResult, err := mediatr.Send[*getProductByIdQueryV1.GetProductById, *getProductByIdDtosV1.GetProductByIdResponseDto](
+		ctx,
+		query,
+	)
+	if err != nil {
+		err = errors.WithMessage(
+			err,
+			"[ProductGrpcServiceServer_GetProductById.Send] error in sending GetProductById",
+		)
+		s.logger.Errorw(
+			fmt.Sprintf(
+				"[ProductGrpcServiceServer_GetProductById.Send] id: {%s}, err: %v",
+				query.ProductID,
+				err,
+			),
+			logger.Fields{"Id": query.ProductID},
+		)
+		return nil, err
+	}
+
+	// Mapear el producto a un DTO de gRPC
+	product, err := mapper.Map[*productsService.Product](queryResult.Product)
+	if err != nil {
+		err = errors.WithMessage(
+			err,
+			"[ProductGrpcServiceServer_GetProductById.Map] error in mapping product",
+		)
+		return nil, err
+	}
+
+	return &productsService.GetProductByIdRes{Product: product}, nil
+}
